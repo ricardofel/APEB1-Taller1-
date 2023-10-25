fe98f19c2a289d061dc69d689ff927816ae725a5
Ricardo Espinosa <rfespinosa7@utpl.edu.ec>
2023-10-24 22:50:13 -0500

    tercer commit, trabajando con Git y GithubClassroom

---
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,7 @@
+package proyecto02;
+public class Proyecto02 {
+    public static void main(String[] args) {
+        System.out.println("Ejemplo dos basico de Java Versionado a traves de Git Manejado desde GitHub ademas se usar GitHubClassroom");
+    }
+    
+}
