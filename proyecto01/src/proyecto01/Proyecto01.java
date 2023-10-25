0b9a051732ec86b6072128c9a040e449cb0c30e5
Ricardo Espinosa <rfespinosa7@utpl.edu.ec>
2023-10-24 22:28:58 -0500

    primer commit

---
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -0,0 +1,7 @@
+package proyecto01;
+public class Proyecto01 {
+    public static void main(String[] args) {
+        System.out.println("Ejemplo basico de Java Versionado a traves de Git Manejado desde GitHub");
+    }
+    
+}
